a416f20160a841051b8e447031eebb117cddfba6
Henrique Ramos <chenrique.ramos12@gmail.com>
2019-04-21 22:58:01 -0300

    Adicionado um Carousel na Home

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { Carousel } from 'antd';
+
+const images = [
+    require('../../assets/carousel/1.jpg'),
+    require('../../assets/carousel/2.jpg'),
+    require('../../assets/carousel/3.jpg'),
+]
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                <h2>Home</h2>
+            <div className="page">
+                <Carousel autoplay>
+                    <div><img src={ images[0] } alt="" className="img-responsive"/></div>
+                    <div><img src={ images[1] } alt="" className="img-responsive"/></div>
+                    <div><img src={ images[2] } alt="" className="img-responsive"/></div>
+                </Carousel>
             </div>
         );
     }
